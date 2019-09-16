b4090e45c51f1f127b09ab85e16f118750dcbba7
Luiz Victor Linhares Rocha <luiz@sigalei.com.br>
2019-09-16 14:51:11 -0300

    refactor(server): trasnfer to class definition

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
 import { GraphQLServer } from "graphql-yoga";
 import { genSchema } from "./modules";
 import { sequelize } from "./services/sequelize";
-import { verifyJwt } from "./middleware/jwt";
 
-export const startServer = () => {
-  const schema = genSchema();
-  const server = new GraphQLServer({
-    schema,
-    context: {
-      sequelize
-    }
-  });
-  server.express.use(verifyJwt);
-  return server;
-};
+class Server extends GraphQLServer {
+  constructor() {
+    const schema = genSchema();
+    super({
+      schema,
+      context: {
+        sequelize
+      }
+    });
+  }
+}
+
+export default new Server();
