d14337f761848f83dd2b2ce5bc899832c8790fc6
Luiz Victor Linhares Rocha <luiz@sigalei.com.br>
2019-09-16 17:55:44 -0300

    fix(jwt): fix verification of jwt

--- src/util/verifyJwt.ts
+++ src/util/verifyJwt.ts
@@ -4,11 +4,11 @@ import * as tp from "typed-promisify";
 
 const verify = tp.promisify(jwt.verify);
 
-export const verifyJwt = async (request: Request): Promise<string | null> => {
+export const verifyJwt = async (request: Request): Promise<any> => {
   const tokenHeader = request.get("x-access-token");
   if (!tokenHeader) {
     return null;
   }
-  const token = tokenHeader.substring(6);
+  const token = tokenHeader.substring(7);
   return verify(token, process.env.APP_SECRET as string) as any;
 };
