51e508400f66e9f19ec9c6c8f7318a1e1f1d7cb2
Luiz Victor Linhares Rocha <luiz@sigalei.com.br>
2019-09-16 17:55:03 -0300

    refactor: remove try catch from mutations

--- src/modules/user/login.ts
+++ src/modules/user/login.ts
@@ -14,42 +14,33 @@ const loginResolver: Resolver = async (
   }: GQL.IUserLoginWithEmailOnMutationArguments,
   { sequelize }
 ): Promise<GQL.IUserLoginWithEmailPayload> => {
-  try {
-    const UserModel = sequelize.models.User;
-    const user = (await UserModel.findOne({ where: { email } })) as User;
-    if (!user) {
-      return {
-        __typename: "UserLoginWithEmailPayload",
-        token: null,
-        error: invalidLogin,
-        clientMutationId: clientMutationId || null
-      };
-    }
-    const valid = await bcrypt.compare(password, user.password);
-    if (valid) {
-      const token = (await sign({ userId: user._id }, process.env
-        .APP_SECRET as string)) as string;
-      return {
-        __typename: "UserLoginWithEmailPayload",
-        token,
-        error: null,
-        clientMutationId: clientMutationId || null
-      };
-    }
+  const UserModel = sequelize.models.User;
+  const user = (await UserModel.findOne({ where: { email } })) as User;
+  if (!user) {
     return {
       __typename: "UserLoginWithEmailPayload",
       token: null,
       error: invalidLogin,
       clientMutationId: clientMutationId || null
     };
-  } catch (err) {
+  }
+  const valid = await bcrypt.compare(password, user.password);
+  if (valid) {
+    const token = (await sign({ userId: user._id }, process.env
+      .APP_SECRET as string)) as string;
     return {
       __typename: "UserLoginWithEmailPayload",
-      token: null,
-      error: err.getMessage,
+      token,
+      error: null,
       clientMutationId: clientMutationId || null
     };
   }
+  return {
+    __typename: "UserLoginWithEmailPayload",
+    token: null,
+    error: invalidLogin,
+    clientMutationId: clientMutationId || null
+  };
 };
 
 export default loginResolver;
--- src/modules/user/register.ts
+++ src/modules/user/register.ts
@@ -1,6 +1,9 @@
 import * as jwt from "jsonwebtoken";
 import { Resolver } from "../../types/graphql-utils";
 import { User } from "../../models/User.model";
+import * as tp from "typed-promisify";
+
+const sign = tp.promisify(jwt.sign);
 
 const registerResolver: Resolver = async (
   _,
@@ -9,29 +12,20 @@ const registerResolver: Resolver = async (
   }: GQL.IUserRegisterWithEmailOnMutationArguments,
   { sequelize }
 ): Promise<GQL.IUserRegisterWithEmailPayload> => {
-  try {
-    const UserModel = sequelize.models.User;
-    const user = (await UserModel.create({
-      email,
-      password,
-      name
-    })) as User;
-    const token = await jwt.sign({ userId: user._id }, process.env
-      .APP_SECRET as string);
-    return {
-      __typename: "UserRegisterWithEmailPayload",
-      token,
-      error: null,
-      clientMutationId: clientMutationId as string | null
-    };
-  } catch (err) {
-    return {
-      __typename: "UserRegisterWithEmailPayload",
-      token: null,
-      error: err.getMessage(),
-      clientMutationId: clientMutationId as string | null
-    };
-  }
+  const UserModel = sequelize.models.User;
+  const user = (await UserModel.create({
+    email,
+    password,
+    name
+  })) as User;
+  const token = (await sign({ userId: user._id }, process.env
+    .APP_SECRET as string)) as string;
+  return {
+    __typename: "UserRegisterWithEmailPayload",
+    token,
+    error: null,
+    clientMutationId: clientMutationId as string | null
+  };
 };
 
 export default registerResolver;
