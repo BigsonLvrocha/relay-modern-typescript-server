39306dd8c58640f5fae220ed985851cd1dd6de6f
Luiz Victor Linhares Rocha <luiz@sigalei.com.br>
2019-09-16 16:09:30 -0300

    fix(jwt): remove jwt middleware

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,26 @@
 import { GraphQLServer } from "graphql-yoga";
 import { genSchema } from "./modules";
 import { sequelize } from "./services/sequelize";
-import { verifyJwt } from "./middleware/jwt";
-
 class Server extends GraphQLServer {
   constructor() {
     const schema = genSchema();
     super({
       schema,
-      context: {
-        sequelize
-      }
+      context: ({ request }) => ({
+        sequelize,
+        request
+      })
     });
     this.middleware();
     this.routes();
   }
 
   middleware() {
-    this.express.use(verifyJwt);
+    // middlewares
   }
 
   routes() {
-    //
+    // routes
   }
 }
 
