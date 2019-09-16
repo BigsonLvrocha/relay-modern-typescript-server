c33a538e6da2bcbb347756a7622dca2d285b54d0
Luiz Victor Linhares Rocha <luiz@sigalei.com.br>
2019-09-16 16:10:18 -0300

    feat(testClient): create test client

---
+++ src/__test__/util/testClient.ts
@@ -0,0 +1,40 @@
+import * as rp from "request-promise";
+import { CookieJar } from "request";
+
+export class TestClient {
+  options: {
+    jar: CookieJar;
+    withCredentials: boolean;
+    json: boolean;
+  };
+
+  constructor(public url: string) {
+    this.options = {
+      withCredentials: true,
+      jar: rp.jar(),
+      json: true
+    };
+  }
+
+  async register(email: string, password: string, name: string) {
+    return rp.post(this.url, {
+      ...this.options,
+      body: {
+        query: `
+        mutation {
+          UserRegisterWithEmail(
+            input: {
+              email: "${email}",
+              password: "${password}",
+              name: "${name}"
+            }
+          ) {
+            token
+            error
+            clientMutationId
+          }
+        }`
+      }
+    });
+  }
+}
