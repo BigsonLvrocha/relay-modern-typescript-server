cd66f965f4a8a051b58c39513ab8e9a480561f70
Luiz Victor Linhares Rocha <luiz@sigalei.com.br>
2019-09-16 16:10:46 -0300

    feat(register): implement register

--- src/modules/user/register.ts
+++ src/modules/user/register.ts
@@ -1,16 +1,37 @@
+import * as jwt from "jsonwebtoken";
 import { Resolver } from "../../types/graphql-utils";
+import { User } from "../../models/User.model";
 
 const registerResolver: Resolver = async (
   _,
-  {  }: GQL.IUserRegisterWithEmailOnMutationArguments,
-  {}
+  {
+    input: { email, name, password, clientMutationId }
+  }: GQL.IUserRegisterWithEmailOnMutationArguments,
+  { sequelize }
 ): Promise<GQL.IUserRegisterWithEmailPayload> => {
-  return {
-    __typename: "UserRegisterWithEmailPayload",
-    token: "123",
-    error: null,
-    clientMutationId: null
-  };
+  try {
+    const UserModel = sequelize.models.User;
+    const user = (await UserModel.create({
+      email,
+      password,
+      name
+    })) as User;
+    const token = await jwt.sign({ userId: user._id }, process.env
+      .APP_SECRET as string);
+    return {
+      __typename: "UserRegisterWithEmailPayload",
+      token,
+      error: null,
+      clientMutationId: clientMutationId as string | null
+    };
+  } catch (err) {
+    return {
+      __typename: "UserRegisterWithEmailPayload",
+      token: null,
+      error: err.getMessage(),
+      clientMutationId: clientMutationId as string | null
+    };
+  }
 };
 
 export default registerResolver;
