4f553e783d2872e3baa77cb53c943c5450716e77
Luiz Victor Linhares Rocha <luiz@sigalei.com.br>
2019-09-19 11:19:43 -0300

    fix(users): fix migration issues with existing records

--- migrations/20190919135209-users_fix.js
+++ migrations/20190919135209-users_fix.js
@@ -2,17 +2,37 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    const promise1 = queryInterface.changeColumn("users", "name", {
-      type: Sequelize.STRING,
-      allowNull: false,
-      unique: true
+    const update1 = queryInterface.bulkUpdate(
+      "users",
+      {
+        active: false
+      },
+      {
+        active: null
+      }
+    );
+    const update2 = queryInterface.bulkUpdate(
+      "users",
+      {
+        name: ""
+      },
+      {
+        name: null
+      }
+    );
+    return Promise.all([update1, update2]).then(() => {
+      const promise1 = queryInterface.changeColumn("users", "name", {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      });
+      const promise2 = queryInterface.changeColumn("users", "active", {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+      });
+      return Promise.all([promise1, promise2]);
     });
-    const promise2 = queryInterface.changeColumn("users", "active", {
-      type: Sequelize.BOOLEAN,
-      allowNull: false,
-      defaultValue: false
-    });
-    return Promise.all([promise1, promise2]);
   },
 
   down: (queryInterface, Sequelize) => {
