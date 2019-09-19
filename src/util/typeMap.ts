c178f1f4c0efabb180615de4e8ce15437e49dfdf
Luiz Victor Linhares Rocha <luiz@sigalei.com.br>
2019-09-19 17:44:54 -0300

    feat(typeMap): add maps for post

--- src/util/typeMap.ts
+++ src/util/typeMap.ts
@@ -23,6 +23,25 @@ export const Post2IPost = (post: Post): Partial<GQL.IPost> => ({
   authorId: post.authorId
 });
 
+export const Post2Edge = (post: Post) => ({
+  node: Post2IPost(post),
+  cursor: post2Cursor(post)
+});
+
+export const post2Cursor = (post: Post) =>
+  string2Cursor(
+    `${post.authorId}||${(post.createdAt as Date).getTime()}`,
+    "post-authorId-createdAt-"
+  );
+
+export const cursor2Post = (cursor: string) => {
+  const data = cursor2String(cursor, "post-authorId-createdAt-").split("||");
+  return {
+    createdAt: new Date(Number.parseInt(data[1], 10)),
+    authorId: data[0]
+  };
+};
+
 export const base64 = (str: string): string =>
   Buffer.from(str, "ascii").toString("base64");
 export const unbase64 = (b64: string): string =>
