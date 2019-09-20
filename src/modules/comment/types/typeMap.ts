12bba8b531d24e582b12e796550a9777b96f1aac
Luiz Victor Linhares Rocha <luiz@sigalei.com.br>
2019-09-20 18:05:07 -0300

    feat(Comment): add type conversions for comment

---
+++ src/modules/comment/types/typeMap.ts
@@ -0,0 +1,47 @@
+import { model2Node, Node2ModelResolver } from "../../../types/graphql-utils";
+import { Comment } from "../../../models/Comment.model";
+import { string2Cursor, cursor2String } from "../../../util/typeMap";
+
+export const idPrefix = "comment";
+export const postCommentCursorPrefix = "post-comment-post-user-createdAt-";
+export const modelName = "Comment";
+export const resolveType = "Comment";
+
+export const comment2IComment: model2Node<Comment, GQL.IComment> = comment => ({
+  _id: comment._id,
+  comment: comment.comment,
+  postId: comment.postId,
+  authorId: comment.authorId
+});
+
+export const comment2PostAuthorEdge = (comment: Comment) => ({
+  node: comment2IComment(comment),
+  cursor: comment2Cursor(comment)
+});
+
+export const comment2Cursor = (comment: Comment) =>
+  string2Cursor(
+    `${comment.postId}||${
+      comment.authorId
+    }||${(comment.createdAt as Date).getTime()}`,
+    postCommentCursorPrefix
+  );
+
+export const cursor2Comment = (cursor: string) => {
+  const data = cursor2String(cursor, postCommentCursorPrefix).split("||");
+  return {
+    createdAt: new Date(Number.parseInt(data[2], 10)),
+    postId: data[0],
+    authorId: data[1]
+  };
+};
+
+export const CommentNode2ModelResolver: Node2ModelResolver<
+  Comment,
+  GQL.IComment
+> = {
+  idPrefix,
+  model2Interface: comment2IComment,
+  modelName: "Comment",
+  resolveType: "Comment"
+};
