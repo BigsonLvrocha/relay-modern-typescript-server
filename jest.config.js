ecdff2a40efa0bcf1ebd4e34fbfd50c5909358ed
Luiz Victor Linhares Rocha <luiz@sigalei.com.br>
2019-09-23 11:36:27 -0300

    config(jest): add setup for test with debug

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   globalSetup: "./src/__test__/setup/setup.ts",
+  setupFilesAfterEnv: ["./src/__test__/setup/setupJest.js"],
   testEnvironment: "node",
   transform: {
     "^.+\\.tsx?$": "ts-jest"
