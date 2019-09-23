482a68cd0d8acf8c48e337e5d602a3ef2e1c83cd
Luiz Victor Linhares Rocha <luiz@sigalei.com.br>
2019-09-23 10:30:46 -0300

    feat(Node): add comment to generic node resolver

--- src/modules/shared/types/typeMaps.ts
+++ src/modules/shared/types/typeMaps.ts
@@ -3,14 +3,18 @@ import { PostNode2ModelResolver } from "../../post/types/typeMap";
 import { Node2ModelResolver } from "../../../types/graphql-utils";
 import { Post } from "../../../models/Post.model";
 import { User } from "../../../models/User.model";
+import { Comment } from "../../../models/Comment.model";
+import { CommentNode2ModelResolver } from "../../comment/types/typeMap";
 
-export type AvailableModel = Post & User;
+export type AvailableModel = Post & User & Comment;
 
 export type AvailableNode2ModelResolver =
   | Node2ModelResolver<Post, GQL.IPost>
-  | Node2ModelResolver<User, GQL.IUser>;
+  | Node2ModelResolver<User, GQL.IUser>
+  | Node2ModelResolver<Comment, GQL.IComment>;
 
 export const AvailableNode2ModelResolvers: AvailableNode2ModelResolver[] = [
   UserNode2ModelResolver,
-  PostNode2ModelResolver
+  PostNode2ModelResolver,
+  CommentNode2ModelResolver
 ];
