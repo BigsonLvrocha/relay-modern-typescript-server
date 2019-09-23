58b62cc84a482c00fedf234ae94adc28cee24e65
Luiz Victor Linhares Rocha <luiz@sigalei.com.br>
2019-09-23 10:13:00 -0300

    feat(commentMigration): add safety constraint to table

--- migrations/20190920201356-comment.js
+++ migrations/20190920201356-comment.js
@@ -2,44 +2,54 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable("comments", {
-      _id: {
-        type: Sequelize.UUID,
-        primaryKey: true
-      },
-      authorId: {
-        type: Sequelize.UUID,
-        references: {
-          model: "users",
-          key: "_id"
+    return queryInterface
+      .createTable("comments", {
+        _id: {
+          type: Sequelize.UUID,
+          primaryKey: true
         },
-        allowNull: false,
-        onDelete: "CASCADE",
-        onUpdate: "CASCADE"
-      },
-      postId: {
-        type: Sequelize.UUID,
-        references: {
-          model: "posts",
-          key: "_id"
+        authorId: {
+          type: Sequelize.UUID,
+          references: {
+            model: "users",
+            key: "_id"
+          },
+          allowNull: false,
+          onDelete: "CASCADE",
+          onUpdate: "CASCADE"
         },
-        allowNull: false,
-        onDelete: "CASCADE",
-        onUpdate: "CASCADE"
-      },
-      comment: {
-        type: Sequelize.TEXT,
-        allowNull: false
-      },
-      createdAt: {
-        type: Sequelize.DATE,
-        allowNull: false
-      },
-      updatedAt: {
-        type: Sequelize.DATE,
-        allowNull: false
-      }
-    });
+        postId: {
+          type: Sequelize.UUID,
+          references: {
+            model: "posts",
+            key: "_id"
+          },
+          allowNull: false,
+          onDelete: "CASCADE",
+          onUpdate: "CASCADE"
+        },
+        comment: {
+          type: Sequelize.TEXT,
+          allowNull: false
+        },
+        createdAt: {
+          type: Sequelize.DATE,
+          allowNull: false
+        },
+        updatedAt: {
+          type: Sequelize.DATE,
+          allowNull: false
+        }
+      })
+      .then(() => {
+        return queryInterface.addConstraint(
+          "comments",
+          ["authorId", "postId", "createdAt"],
+          {
+            type: "unique"
+          }
+        );
+      });
   },
 
   down: (queryInterface, Sequelize) => {
